b8c7c682f197f9db0e1a0659e6003b0cca9346f1
Nicole Peralta <89421724+nicolescania@users.noreply.github.com>
2023-01-16 23:25:25 -0500

    Web Application Development / Lab01

---
+++ Lab01/Modules/main.js
@@ -0,0 +1,19 @@
+import {halfOf} from './lib.js';
+
+import doSomething from './doSomething.js';
+
+
+import { flag, touch } from './validator.js';
+
+console.log(flag);
+
+touch();
+
+doSomething();
+
+console.log(halfOf(84));
+
+console.log(multiply(21, 2));
+
+console.log(flag);
+
---
+++ Lab01/REST/Rest.js
@@ -0,0 +1,9 @@
+function userFriends(user, ...friends) {
+
+    console.log(user + ' has ' + friends.length + ' friends');
+
+ }
+
+ userFriends('User', 'Bob', 'Alice');
+
+ 
